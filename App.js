15b7584ad1a97fa71f4702e46fed89d49d3513b9
“T3sultan” <incognito.soul011@gmail.com>
2022-07-29 14:46:11 +0600

    dfd

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#000",
 
     color: "#ffff",
     alignItems: "center",
