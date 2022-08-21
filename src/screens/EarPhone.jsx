6fdc171421db854ec101cacf3fa6370ceb56cb67
“T3sultan” <incognito.soul011@gmail.com>
2022-08-21 23:39:18 +0600

    speakers and earphones

--- src/screens/EarPhone.jsx
+++ src/screens/EarPhone.jsx
@@ -1,15 +1,75 @@
-import { StyleSheet, View } from "react-native";
+import { Image, ScrollView, StyleSheet, View } from "react-native";
 import React from "react";
 import Text from "../common/text";
+import { useSelector } from "react-redux";
+import { selectEarPhones } from "../redux/productSlice";
+import BannerTitle from "../common/BannerTitle";
+import CatagoryTitle from "../components/CatagoryTitle";
+import { colors, spacing } from "../theme";
+import CustomButton from "../common/CustomButton";
+import Footer from "../common/Footer";
 
 const EarPhone = () => {
+  const earphones = useSelector(selectEarPhones);
   return (
-    <View>
-      <Text>EarPhone</Text>
+    <View style={styles.container}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <BannerTitle />
+        <CatagoryTitle title={"Earphones"} />
+        <View style={styles.wrapper}>
+          {earphones.map(earphone => {
+            return (
+              <View key={earphone.name} style={{ marginBottom: spacing[9] }}>
+                <View style={styles.wrapper1}>
+                  <Image source={earphone.featuredImage.source} />
+                </View>
+                <View style={{ marginTop: spacing[5] }}>
+                  <Text centered preset="h5">
+                    {earphone.name}
+                  </Text>
+                  <Text centered preset="h6">
+                    earphones
+                  </Text>
+                  <Text
+                    centered
+                    textColor="#919191"
+                    style={{
+                      marginTop: spacing[4],
+                      marginHorizontal: spacing[4],
+                    }}
+                  >
+                    {earphone.description}
+                  </Text>
+                </View>
+                <View style={{ marginTop: spacing[2] }}>
+                  <CustomButton
+                    onPress={() => {}}
+                    purple
+                    title={"SEE PRODUCT"}
+                  />
+                </View>
+              </View>
+            );
+          })}
+        </View>
+        <Footer />
+      </ScrollView>
     </View>
   );
 };
 
 export default EarPhone;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {},
+  wrapper: {
+    margin: spacing[5],
+  },
+  wrapper1: {
+    backgroundColor: colors.gry,
+    borderRadius: spacing[4],
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: spacing[5],
+  },
+});
--- src/screens/Speakers.jsx
+++ src/screens/Speakers.jsx
@@ -1,15 +1,75 @@
-import { StyleSheet, View } from "react-native";
+import { Image, ScrollView, StyleSheet, View } from "react-native";
 import React from "react";
 import Text from "../common/text";
+import { useSelector } from "react-redux";
+import { selectSpeakers } from "../redux/productSlice";
+import BannerTitle from "../common/BannerTitle";
+import CatagoryTitle from "../components/CatagoryTitle";
+import { colors, spacing } from "../theme";
+import CustomButton from "../common/CustomButton";
+import Footer from "../common/Footer";
 
-const Speakers = ({ navigation }) => {
+const Speakers = () => {
+  const speakers = useSelector(selectSpeakers);
   return (
-    <View>
-      <Text>Speakers</Text>
+    <View style={styles.container}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <BannerTitle />
+        <CatagoryTitle title={"Earphones"} />
+        <View style={styles.wrapper}>
+          {speakers.map(speaker => {
+            return (
+              <View key={speaker.name} style={{ marginBottom: spacing[9] }}>
+                <View style={styles.wrapper1}>
+                  <Image source={speaker.featuredImage.source} />
+                </View>
+                <View style={{ marginTop: spacing[5] }}>
+                  <Text centered preset="h5">
+                    {speaker.name}
+                  </Text>
+                  <Text centered preset="h6">
+                    speakers
+                  </Text>
+                  <Text
+                    centered
+                    textColor="#919191"
+                    style={{
+                      marginTop: spacing[4],
+                      marginHorizontal: spacing[4],
+                    }}
+                  >
+                    {speaker.description}
+                  </Text>
+                </View>
+                <View style={{ marginTop: spacing[2] }}>
+                  <CustomButton
+                    onPress={() => {}}
+                    purple
+                    title={"SEE PRODUCT"}
+                  />
+                </View>
+              </View>
+            );
+          })}
+        </View>
+        <Footer />
+      </ScrollView>
     </View>
   );
 };
 
 export default Speakers;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {},
+  wrapper: {
+    margin: spacing[5],
+  },
+  wrapper1: {
+    backgroundColor: colors.gry,
+    borderRadius: spacing[4],
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: spacing[5],
+  },
+});
